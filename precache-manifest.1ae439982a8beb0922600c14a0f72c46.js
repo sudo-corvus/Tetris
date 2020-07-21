5bad843cdbb5ec0b28945951cedc4bb09f4b3bd0
sudo-corvus <neo11prince@gmail.com>
2020-07-21 21:38:28 +0530

    Updates

---
+++ precache-manifest.1ae439982a8beb0922600c14a0f72c46.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b7c92f9165b9c37b1563261fd1e24e35",
+    "url": "/Tetris/index.html"
+  },
+  {
+    "revision": "ce39a11634424dea7c31",
+    "url": "/Tetris/static/css/main.66dc15d2.chunk.css"
+  },
+  {
+    "revision": "772a4c508f6f4df91ed4",
+    "url": "/Tetris/static/js/2.5c94cc50.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Tetris/static/js/2.5c94cc50.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ce39a11634424dea7c31",
+    "url": "/Tetris/static/js/main.ef3b2c66.chunk.js"
+  },
+  {
+    "revision": "bc32f3af2d9052356c66",
+    "url": "/Tetris/static/js/runtime-main.a7b216e0.js"
+  },
+  {
+    "revision": "e28975521df42a470a60593872337098",
+    "url": "/Tetris/static/media/Pixel-LCD-7.e2897552.woff"
+  },
+  {
+    "revision": "2810fe6b5435e75b3dad63e341b97d02",
+    "url": "/Tetris/static/media/bg.2810fe6b.png"
+  }
+]);
